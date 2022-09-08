31ba8bc31f8929812bbaf51563e1be26fd646179
student <student@C246U30>
2022-09-08 09:58:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>      //For I/O Functions
+#include <windows.h>   //For Beep & Sleep functions
+#include<stdlib.h>  //For System Pause
+using namespace std;
+
+int main()
+{
+
+    system("pause");
+    return 0;
+}   //Press any key to exit
